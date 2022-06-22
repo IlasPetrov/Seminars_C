8161421a1ceae8b31831056919998d7ec9be2311
IPetrov <tpl45@mail.ru>
2022-06-23 02:19:55 +0300

    First seminar completed

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+int num, result;
+
+Console.Write("Введите целое число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+result = num * num;
+
+Console.WriteLine("Result: " + result);
+
+*/
+/*
+
+int numA, numB, quad;
+
+Console.Write("Введите первое число: ");
+numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+numB = Convert.ToInt32(Console.ReadLine());
+
+quad = numB * numB;
+
+if(numA == quad)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+*/
+/*
+
+int num;
+
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++; //current = current + 1;
+}
+
+*/
