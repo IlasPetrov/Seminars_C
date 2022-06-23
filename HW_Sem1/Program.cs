f7554841909b19d83397e70712595b6d1ab4ecf7
IPetrov <tpl45@mail.ru>
2022-06-23 05:15:38 +0300

    Решение задачи № 4

--- HW_Sem1/Program.cs
+++ HW_Sem1/Program.cs
@@ -1,5 +1,5 @@
-﻿// Задача №2
-/*
+﻿/*
+// Задача №2
 
 Console.Write("Введите первое число: ");
 int numA = Convert.ToInt32(Console.ReadLine());
@@ -14,9 +14,28 @@ if (numB > max) max = numB;
 Console.WriteLine("MAX = " + max);
 
 */
+/*
+// Задача №4
 
 
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+
+Console.WriteLine("" + max);
+
+*/
 
 
 
